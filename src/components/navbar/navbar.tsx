8179e968824d1e553960ddca2295e05436f23ce3
soyeong <psykor48@gmail.com>
2024-02-18 18:36:02 +0900

    navbar utilmenu list 추가

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,17 +1,30 @@
+'use client';
 import Link from 'next/link';
 import styles from './navbar.module.css';
+import { usePathname } from 'next/navigation';
+
+
+const navList = [
+  { text: '시공 견적후기', href: '/boards' },
+  { text: '인테리어 이야기', href: '/community' },
+]
 
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <header className={styles.header}>
       <h1>
         <Link href="/">LOGO</Link>
       </h1>
       <ul className={styles.navbar}>
-        <li></li>
-        <li><Link href="/boards">시공 견적후기</Link></li>
-        <li><Link href="/community">인테리어 이야기</Link></li>
+        {
+          navList.map(({ text, href }) => (
+            <li className={`${pathname === href && styles.navbarClicked}`}>
+              <Link href={href}>{text}</Link>
+            </li>
+          ))
+        }
       </ul>
       <ul className={styles.util}>
         <li><button type="button">찾기</button></li>
